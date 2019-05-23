38a47abbb159a4b10f63e9982638e467a8cd6f1e
priyansh35 <priyansh.linuxbean@gmail.com>
2019-05-23 20:06:19 +0530

    add doughnut chart

--- src/components/dashboard/Resources.js
+++ src/components/dashboard/Resources.js
@@ -1,14 +1,86 @@
 import React, { Component } from 'react';
+import { Doughnut } from 'react-chartjs-2';
 class Resources extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            chartData: props.chartData
+        }
     }
-
+    componentWillMount() {
+        this.getChartData();
+    }
+    getChartData() {
+        this.setState({
+            chartData: {
+                labels: [],
+                datasets: [
+                    {
+                        label: 'Population',
+                       
+                        data: [
+                            10,
+                            30,
+                            80,
+                        ],
+                        backgroundColor: [
+                            '#f5c205',
+                            '#4dc175',
+                            '#2f36f0',
+                           
+                        ]
+                    }
+                ]
+            },
+            chartData2: {
+                labels: [],
+                datasets: [
+                    {
+                        label: 'Population',
+                        data: [
+                            80,
+                            15,
+                            20,
+                        ],
+                        backgroundColor: [
+                            '#f5c205',
+                            '#4dc175',
+                            '#2f36f0',
+                           
+                        ]
+                    }
+                ]
+            },
+            chartData3: {
+                labels: [],
+                datasets: [
+                    {
+                        label: 'Population',
+                        data: [
+                            15,
+                            80,
+                            30,
+                        ],
+                        backgroundColor: [
+                            '#f5c205',
+                            '#4dc175',
+                            '#2f36f0',
+                           
+                        ]
+                    }
+                ]
+            }
+        });
+    }
     render() {
+
         return (
             <div >
-               <div className="col-md-3 col-sm-3">
+                <div className="chart">
+
+                </div>
+                <div className="col-md-3 col-sm-3">
                     <div className="dash-main-box">
                         <h3>Resources <a href="#"><i className="fa fa-pencil"></i></a></h3>
                         <div className="dash-inner-box">
@@ -17,106 +89,233 @@ class Resources extends Component {
                                 <div className="circle-graph-box" id="style-2">
                                     <div className="circle-graph-box-inner">
                                         <h6>Title I</h6>
-                                        <img src="/img/gr1.png"/>
+                                        <Doughnut
+                                            ref={(reference) => this.chartReference = reference}
+                                            data={this.state.chartData}
+
+                                            options={{ maintainAspectRatio: false, responsive: true,tooltips: {
+                                                mode: false
+                                            } }}
+                                            
+                                        />
                                     </div>
 
                                     <div className="circle-graph-box-inner">
                                         <h6>Title II A</h6>
-                                        <img src="/img/gr2.png"/>
+                                        <Doughnut
+                                            ref={(reference) => this.chartReference = reference}
+                                            data={this.state.chartData}
+
+                                            options={{ maintainAspectRatio: false, responsive: true,tooltips: {
+                                                mode: false
+                                            } }}
+                                        />
                                     </div>
-                                    
+
                                     <div className="circle-graph-box-inner">
                                         <h6>Title I</h6>
-                                        <img src="/img/gr3.png"/>
+                                        <Doughnut
+                                            ref={(reference) => this.chartReference = reference}
+                                            data={this.state.chartData}
+
+                                            options={{ maintainAspectRatio: false, responsive: true,tooltips: {
+                                                mode: false
+                                            } }}
+                                        />
                                     </div>
 
                                     <div className="circle-graph-box-inner">
                                         <h6>Title II A</h6>
-                                        <img src="/img/gr4.png"/>
+                                        <Doughnut
+                                            ref={(reference) => this.chartReference = reference}
+                                            data={this.state.chartData}
+
+                                            options={{ maintainAspectRatio: false, responsive: true,tooltips: {
+                                                mode: false
+                                            } }}
+                                        />
                                     </div>
-                                    
+
                                     <div className="circle-graph-box-inner">
                                         <h6>Title I</h6>
-                                        <img src="/img/gr5.png"/>
+                                        <Doughnut
+                                            ref={(reference) => this.chartReference = reference}
+                                            data={this.state.chartData}
+
+                                            options={{ maintainAspectRatio: false, responsive: true,tooltips: {
+                                                mode: false
+                                            } }}
+                                        />
                                     </div>
 
                                     <div className="circle-graph-box-inner">
                                         <h6>Title II A</h6>
-                                        <img src="/img/gr6.png"/>
+                                        <Doughnut
+                                            ref={(reference) => this.chartReference = reference}
+                                            data={this.state.chartData}
+
+                                            options={{ maintainAspectRatio: false, responsive: true,tooltips: {
+                                                mode: false
+                                            } }}
+                                        />
                                     </div>
-                                    
+
                                 </div>
                             </div>
-                            
+
                             <div className="circle-graph-main">
                                 <h5>State Funds</h5>
                                 <div className="circle-graph-box" id="style-2">
                                     <div className="circle-graph-box-inner">
                                         <h6>LCFF</h6>
-                                        <img src="/img/gr3.png"/>
+                                        <Doughnut
+                                            ref={(reference) => this.chartReference = reference}
+                                            data={this.state.chartData2}
+
+                                            options={{ maintainAspectRatio: false, responsive: true,tooltips: {
+                                                mode: false
+                                            } }}
+                                        />
                                     </div>
 
                                     <div className="circle-graph-box-inner">
                                         <h6>Title II A</h6>
-                                        <img src="/img/gr4.png"/>
+                                        <Doughnut
+                                            ref={(reference) => this.chartReference = reference}
+                                            data={this.state.chartData2}
+
+                                            options={{ maintainAspectRatio: false, responsive: true,tooltips: {
+                                                mode: false
+                                            } }}
+                                        />
                                     </div>
-                                    
+
                                     <div className="circle-graph-box-inner">
                                         <h6>Title I</h6>
-                                        <img src="/img/gr5.png"/>
+                                        <Doughnut
+                                            ref={(reference) => this.chartReference = reference}
+                                            data={this.state.chartData2}
+
+                                            options={{ maintainAspectRatio: false, responsive: true,tooltips: {
+                                                mode: false
+                                            } }}
+                                        />
                                     </div>
 
                                     <div className="circle-graph-box-inner">
                                         <h6>Title II A</h6>
-                                        <img src="/img/gr6.png"/>
+                                        <Doughnut
+                                            ref={(reference) => this.chartReference = reference}
+                                            data={this.state.chartData2}
+
+                                            options={{ maintainAspectRatio: false, responsive: true,tooltips: {
+                                                mode: false
+                                            } }}
+                                        />
                                     </div>
-                                    
+
                                     <div className="circle-graph-box-inner">
                                         <h6>Title I</h6>
-                                        <img src="/img/gr1.png"/>
+                                        <Doughnut
+                                            ref={(reference) => this.chartReference = reference}
+                                            data={this.state.chartData2}
+
+                                            options={{ maintainAspectRatio: false, responsive: true,tooltips: {
+                                                mode: false
+                                            } }}
+                                        />
                                     </div>
 
                                     <div className="circle-graph-box-inner">
                                         <h6>Title II A</h6>
-                                        <img src="/img/gr2.png"/>
+                                        <Doughnut
+                                            ref={(reference) => this.chartReference = reference}
+                                            data={this.state.chartData2}
+
+                                            options={{ maintainAspectRatio: false, responsive: true,tooltips: {
+                                                mode: false
+                                            } }}
+                                        />
                                     </div>
-                                    
+
                                 </div>
                             </div>
-                            
+
                             <div className="circle-graph-main">
                                 <h5>Local Funds</h5>
                                 <div className="circle-graph-box" id="style-2">
                                     <div className="circle-graph-box-inner">
                                         <h6>Parcel Tax</h6>
-                                        <img src="/img/gr5.png"/>
+                                        <Doughnut
+                                            ref={(reference) => this.chartReference = reference}
+                                            data={this.state.chartData3}
+
+                                            options={{ maintainAspectRatio: false, responsive: true,tooltips: {
+                                                mode: false
+                                            } }}
+                                        />
                                     </div>
 
                                     <div className="circle-graph-box-inner">
                                         <h6>Local Foundations</h6>
-                                        <img src="/img/gr6.png"/>
+                                        <Doughnut
+                                            ref={(reference) => this.chartReference = reference}
+                                            data={this.state.chartData3}
+
+                                            options={{ maintainAspectRatio: false, responsive: true,tooltips: {
+                                                mode: false
+                                            } }}
+                                        />
                                     </div>
-                                    
+
                                     <div className="circle-graph-box-inner">
                                         <h6>Title I</h6>
-                                        <img src="/img/gr1.png"/>
+                                        <Doughnut
+                                            ref={(reference) => this.chartReference = reference}
+                                            data={this.state.chartData3}
+
+                                            options={{ maintainAspectRatio: false, responsive: true,tooltips: {
+                                                mode: false
+                                            } }}
+                                        />
                                     </div>
 
                                     <div className="circle-graph-box-inner">
                                         <h6>Title II A</h6>
-                                        <img src="/img/gr2.png"/>
+                                        <Doughnut
+                                            ref={(reference) => this.chartReference = reference}
+                                            data={this.state.chartData3}
+
+                                            options={{ maintainAspectRatio: false, responsive: true,tooltips: {
+                                                mode: false
+                                            } }}
+                                        />
                                     </div>
-                                    
+
                                     <div className="circle-graph-box-inner">
                                         <h6>Title I</h6>
-                                        <img src="/img/gr3.png"/>
+                                        <Doughnut
+                                            ref={(reference) => this.chartReference = reference}
+                                            data={this.state.chartData3}
+
+                                            options={{ maintainAspectRatio: false, responsive: true,tooltips: {
+                                                mode: false
+                                            } }}
+                                        />
                                     </div>
 
                                     <div className="circle-graph-box-inner">
                                         <h6>Title II A</h6>
-                                        <img src="/img/gr4.png"/>
+                                        <Doughnut
+                                            ref={(reference) => this.chartReference = reference}
+                                            data={this.state.chartData3}
+
+                                            options={{ maintainAspectRatio: false, responsive: true,tooltips: {
+                                                mode: false
+                                            } }}
+                                        />
                                     </div>
-                                    
+
                                 </div>
                             </div>
                         </div>
